a0b6a0e213f3cbc717fd79b531809a53fd6a71eb
Dan Onoshko <d.onoshko.student@sibneuro.ru>
2014-09-24 00:46:16 +0700

    move license to MIT

--- loader.js
+++ loader.js
@@ -1,20 +1,6 @@
 /*
 
 wakeloader - scripts loader and main function caller for JavaScript. recommend only for developing.
-Copyright (C) 2013 TrigenSoftware
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 Props:
     string    main-file    path to main.js file
